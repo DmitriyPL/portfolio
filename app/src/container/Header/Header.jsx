d8e4d8948799256dc485fe23d2348d28b3e91d7f
plhome <plhome@yandex.ru>
2023-01-22 17:57:49 +0300

    test webhook

--- app/src/container/Header/Header.jsx
+++ app/src/container/Header/Header.jsx
@@ -32,12 +32,12 @@ const Header = () => {
                     <img src={images.hello} alt='hello' />
                     <div style={{ marginLeft: 20 }}>
                         <p className='p-text'>Hello, I am</p>
-                        <h1 className='head-text'>Dmitriy</h1>
+                        <h1 className='head-text'>Dmitri</h1>
                     </div>
                 </div>
                 <div className='tag-cmp app__flex'>
                     <p className='p-text'>Web Developer</p>
-                    <p className='p-text'>Freelancer</p>   
+                    <p className='p-text'>Freelancer</p>
                 </div>
             </div>
         </motion.div>
@@ -45,15 +45,15 @@ const Header = () => {
         <motion.div
             whileInView={{ opacity: [0, 1] }}
             transition={{ duration: 0.5, delayChildren: 0.5 }}
-            className="app__header-img"        
+            className="app__header-img"
         >
             <img src={images.profile} alt="profile_bg" />
             <motion.img
                 whileInView={{ scale: [0, 1] }}
-                transition={{ duration: 1, ease: 'easeInOut' }}   
+                transition={{ duration: 1, ease: 'easeInOut' }}
                 src={images.circle}
                 alt='profile_circle'
-                className="overlay_circle" 
+                className="overlay_circle"
             />
         </motion.div>
 
